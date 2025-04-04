c643fb1c79d13df4a21520caa3e58241fa45f4f4
Saifullah Khan <saifullah.korai.15@gmail.com>
2025-04-04 17:17:51 +0500

    initial commit of this learning

---
+++ day_5/03_operators.py
@@ -0,0 +1,51 @@
+# # This program is for the Understanding of Operators
+
+# # Here are some operators
+
+# # integer operators
+# x = 16
+
+# y = 5
+
+# # float operators
+# z = 5.5
+
+# #String
+# a = "Codanics "
+# b = "Pakistan"
+
+
+# # displaying as it is the value of 'a' variable
+# print( a )
+
+# # two strings values will be written as it is "a" value and "b" value
+# print(a+b)
+
+
+# # Arithmetic Operators
+
+# print( x + y ) # addition
+# print(x-y) # Subtraction
+# print(x - z) # subtraction 
+# print(x / y) # Division
+# print(x * y) # multiplication
+# print(x % y) # Remainder
+
+# print(x - z * 4 / y + 3) # DMAS # PEMDAS #BODMAS
+
+
+# x = 11
+# y = 3
+# z = 5.2
+# # Comparison operator
+
+# print( x > y ) # x greater than y
+# print( x < y ) # x less than y
+# print( x == y) # x equal to y
+# print( x <= y) # x less than or equals to y
+# print( x >= y) # x greater than or equal to y
+# print(x != y ) # x does not equal to y
+
+
+
+
